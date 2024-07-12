383c9e055922e5bf7f342176caa160b239b3931d
Cindy Pham <phamcindy619@utexas.edu>
2024-07-12 17:22:12 -0500

    Connect backend to Firestore and implement adding and retrieving projects

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+import firebase_admin
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("firebase_sdk_key.json")
+firebase_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = Flask(__name__)
+
+@app.route('/api/projects', methods=['GET'])
+def home():
+    projects_ref = db.collection('projects')
+    projects = projects_ref.stream()
+    res = []
+    for proj in projects:
+        res.append(proj.to_dict())
+    return jsonify(res)
+
+@app.route('/api/project', methods=['POST'])
+def handle_project_data():
+    if request.method == 'POST':
+        data = request.form
+
+        response = {
+            'title': data.get('title'),
+            'location': data.get('location'),
+            'start_date': data.get('start_date'),
+            'end_date': data.get('end_date'),
+            'description': data.get('description')
+        }
+
+        db.collection('projects').add(response)
+
+        return jsonify(response)
+    
+
+if __name__ == "__main__":
+    app.run()
