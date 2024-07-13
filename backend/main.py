a3bbefaa48aeafdb1bad7a0c2187e2934c3814d5
Cindy Pham <phamcindy619@utexas.edu>
2024-07-12 20:00:23 -0500

    Recreate frontend to Next.js app

--- backend/main.py
+++ backend/main.py
@@ -40,4 +40,4 @@ def save_project():
     
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)
--- frontend/run_frontend.sh
+++ frontend/run_frontend.sh
@@ -1,2 +1,3 @@
 echo "Running frontend..."
 sudo docker build -t frontend .
+sudo docker run -p 3000:3000 frontend
